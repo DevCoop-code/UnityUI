44d1a4b26f8933bf97ea1f0e624647b40e21bb5d
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-09-01 12:11:55 +0900

    feat: Add Event Trigger in Image Component

---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void eventTrigger(){
+		Debug.Log ("Event is occured!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
