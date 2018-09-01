aab77e2a46d3e517fe48841ba479505ba8f588ec
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-09-01 12:33:46 +0900

    feat: UISlider Event

---
+++ Assets/Scripts/SliderEventScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderEventScript : MonoBehaviour {
+
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Mathf.MoveTowards (slider.value, 1, 0.001f);
+	}
+}
